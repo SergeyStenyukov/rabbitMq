e74beb2a1cdc355cf602c6007dfae5be43173997
Sergey Stenyukov <stenyukovs@gmail.com>
2022-08-02 10:14:01 +0300

    anonymous auto-delete queue

--- consumer/src/main/java/example/listener/Listener.java
+++ consumer/src/main/java/example/listener/Listener.java
@@ -1,6 +1,9 @@
 package example.listener;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,17 +12,24 @@ public class Listener {
 
     private final String DEFAULT_DIRECT = "default_direct";
 
-    private final String FIRST_DIRECT = "first_direct";
+    private final String FIRST_KEY = "first";
 
-    private final String SECOND_DIRECT = "second_direct";
+    private final String SECOND_KEY = "second";
 
     @RabbitListener(containerFactory = "defaultQueue", queues = {DEFAULT_DIRECT})
     public void readMessage(Message message){
         System.out.println("MESSAGE READ from " + DEFAULT_DIRECT + ", message: " + message);
     }
 
-    @RabbitListener(containerFactory = "directExchange", queues = {FIRST_DIRECT, SECOND_DIRECT})
-    public void readMessageFromDirect(Message message){
-        System.out.println("MESSAGE READ from direct exchange, message: " + message);
+    @RabbitListener(containerFactory = "directExchange", bindings = @QueueBinding(exchange = @Exchange("direct_exchange"),
+            key = FIRST_KEY, value = @Queue("")))
+    public void readMessageFromDirectFirst(Message message){
+        System.out.println("MESSAGE READ from direct exchange, key first, message: " + message);
+    }
+
+    @RabbitListener(containerFactory = "directExchange", bindings = @QueueBinding(exchange = @Exchange("direct_exchange"),
+            key = SECOND_KEY, value = @Queue("")))
+    public void readMessageFromDirectSecond(Message message){
+        System.out.println("MESSAGE READ from direct exchange, key second, message: " + message);
     }
 }
